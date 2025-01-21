ad458d943a02b3549dbf2479757fb911727055dc
Layne Hsu <45862838+DawnSaint@users.noreply.github.com>
2025-01-21 17:00:34 +0800

    Update mcr.py

--- mcr.py
+++ mcr.py
@@ -1,55 +1,51 @@
-def is_win(game):
-    win = False
-    # Check rows
-    if game[0][0] == game[0][1] == game[0][2] and (game[0][0] == 'X' or game[0][0] == 'O'):
-        win = True
-    if game[1][0] == game[1][1] == game[1][2] and (game[1][0] == 'X' or game[1][0] == 'O'):
-        win = True
-    if game[2][0] == game[2][1] == game[2][2] and (game[2][0] == 'X' or game[2][0] == 'O'):
-        win = True
-    # Check columns
-    if game[0][0] == game[1][0] == game[2][0] and (game[0][0] == 'X' or game[0][0] == 'O'):
-        win = True
-    if game[0][1] == game[1][1] == game[2][1] and (game[0][1] == 'X' or game[0][1] == 'O'):
-        win = True
-    if game[0][2] == game[1][2] == game[2][2] and (game[0][2] == 'X' or game[0][2] == 'O'):
-        win = True
-    # Check diagonals
-    if game[0][0] == game[1][1] == game[2][2] and (game[0][0] == 'X' or game[0][0] == 'O'):
-        win = True
-    if game[0][2] == game[1][1] == game[2][0] and (game[0][2] == 'X' or game[0][2] == 'O'):
-        win = True
-    return win
+def check_winner(board, mark):
+    # Check rows, columns and diagonals for a win
+    for i in range(3):
+        if all([cell == mark for cell in board[i]]):  # Rows
+            return True
+        if all([board[j][i] == mark for j in range(3)]):  # Columns
+            return True
+    # Diagonals
+    if all([board[i][i] == mark for i in range(3)]) or \
+       all([board[i][2-i] == mark for i in range(3)]):
+        return True
+    return False
+
+def is_board_full(board):
+    return all(all(cell != ' ' for cell in row) for row in board)
+
+def display_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+
+def get_valid_move(board):
+    while True:
+        try:
+            move = tuple(map(int, input("Enter move (row column): ").split()))
+            if len(move) == 2 and all(0 <= m < 3 for m in move) and board[move[0]][move[1]] == ' ':
+                return move
+        except ValueError:
+            pass
+        print("Invalid move. Try again.")
 
 def main():
-    game = [[' ' for _ in range(3)] for _ in range(3)]  # Tic-tac-toe board
-    player1 = 'X'
-    player2 = 'O'
-    turn = False  # False for player 1's turn, True for player 2's turn. Player 1 first.
-    print("X = Player 1")
-    print("O = Player 2")
-    for n in range(9):
-        turn = not turn  # Switch turns
-        if not turn:
-            print("Player 1: ", end="")
-        else:
-            print("Player 2: ", end="")
-        print("Which cell to mark? i:[1..3], j:[1..3]: ")
-        i, j = map(int, input().split())
-        i -= 1
-        j -= 1
-        if not turn:
-            game[i][j] = 'X'
-        else:
-            game[i][j] = 'O'
-        if is_win(game):
-            print("Win!")
-            break  # Terminate the game
-        if n == 8:  # All cells have been filled
-            print("Tie!")
-        # Show the game board
-        for row in game:
-            print(" ".join(row))
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+    while True:
+        display_board(board)
+        print(f"Player {current_player}'s turn.")
+        row, col = get_valid_move(board)
+        board[row][col] = current_player
+        if check_winner(board, current_player):
+            display_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        elif is_board_full(board):
+            display_board(board)
+            print("It's a tie!")
+            break
+        current_player = 'O' if current_player == 'X' else 'X'
 
 if __name__ == "__main__":
     main()
